93b21b819e079afb873521433f83e53b273208f5
Petteri Pesonen <petteri.pesonen@karttakeskus.fi>
2017-07-18 12:38:45 +0300

    Small changes

--- handlers/index.py
+++ handlers/index.py
@@ -17,6 +17,7 @@ class IndexHandler(tornado.web.RequestHandler):
             title="GTFS Viewer",
         )
 
+
 class Routes(tornado.web.RequestHandler):
     def get(self):
         self.render(
@@ -30,7 +31,7 @@ class Routes(tornado.web.RequestHandler):
         queryParam2 = self.get_argument("routeID").encode('utf-8')
         queryParam3 = self.get_argument("routeProvider").encode('utf-8')
         queryParam4 = self.get_argument("routeShort").encode('utf-8')
-        response = executeRouteQuery(queryParam1, queryParam2, queryParam3, queryParam4)
+        response = self.executeRouteQuery(queryParam1, queryParam2, queryParam3, queryParam4)
         data = []
         for a, b, c, d, e in response:
             item = {}
@@ -45,6 +46,28 @@ class Routes(tornado.web.RequestHandler):
 
         self.write(json.dumps(data))
 
+    def executeRouteQuery(self, queryParam1, queryParam2, queryParam3, queryParam4):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+
+        try:
+            cursorobj.execute("""
+                SELECT route_id, agency_name, route_short_name, route_long_name, agency_url
+                FROM routes
+                INNER JOIN agency ON routes.agency_id = agency.agency_id
+                WHERE route_long_name LIKE '%{0}%' AND route_id LIKE '%{1}%' AND agency_name LIKE '%{2}%' AND route_short_name LIKE '%{3}%'
+                ORDER BY route_id
+            """.format(queryParam1, queryParam2, queryParam3, queryParam4))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+            raise
+
+        connection.close()
+        return result
+
+
 class Stops(tornado.web.RequestHandler):
     def get(self):
         self.render(
@@ -56,7 +79,7 @@ class Stops(tornado.web.RequestHandler):
     def post(self):
         queryParam1 = self.get_argument("stopID").encode('utf-8')
         queryParam2 = self.get_argument("stopName").encode('utf-8')
-        response = executeStopQuery(queryParam1, queryParam2)
+        response = self.executeStopQuery(queryParam1, queryParam2)
         data = []
         for a, b, c, d in response:
             item = {}
@@ -70,10 +93,31 @@ class Stops(tornado.web.RequestHandler):
 
         self.write(json.dumps(data))
 
+    def executeStopQuery(self, queryParam1, queryParam2):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+
+        try:
+            cursorobj.execute("""
+                SELECT stop_id, stop_name, stop_lat, stop_lon
+                FROM stops
+                WHERE stop_id LIKE '%{0}%' AND stop_name LIKE '%{1}%'
+                ORDER BY stop_id
+            """.format(queryParam1, queryParam2))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+                raise
+
+        connection.close()
+        return result
+
+
 class Trips(tornado.web.RequestHandler):
     def post(self):
         queryParam = self.get_argument("routeID").encode('utf-8')
-        response = executeTripQuery(queryParam)
+        response = self.executeTripQuery(queryParam)
         data = []
         for a, b in response:
             item = {}
@@ -83,12 +127,33 @@ class Trips(tornado.web.RequestHandler):
 
         self.write(json.dumps(data))
 
+    def executeTripQuery(self, queryParam):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+
+        try:
+            cursorobj.execute("""
+                SELECT trips.trip_id, trips.trip_headsign
+                FROM trips
+                INNER JOIN routes ON routes.route_id = trips.route_id
+                WHERE trips.route_id IS '{0}'
+            """.format(queryParam))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+            raise
+
+        connection.close()
+        return result
+
+
 class TripStops(tornado.web.RequestHandler):
     def post(self):
         queryParam = self.get_argument("tripID").encode('utf-8')
-        response = executeTripStopsQuery(queryParam)
+        response = self.executeTripStopsQuery(queryParam)
         data = []
-        for a, b, c, d, e, f in response:
+        for a, b, c, d, e, f, g, h, i in response:
             item = {}
             item['stop_id'] = a
             item['stop_name'] = b
@@ -96,14 +161,38 @@ class TripStops(tornado.web.RequestHandler):
             item['stop_lon'] = d
             item['stop_sequence'] = e
             item['shape_dist_traveled'] = f
+            item['trip_id'] = g
+            item['arrival_time'] = h
+            item['departure_time'] = i
             data.append(item)
 
         self.write(json.dumps(data))
 
+    def executeTripStopsQuery(self, queryParam):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+
+        try:
+            cursorobj.execute("""
+                SELECT stops.stop_id, stops.stop_name, stops.stop_lat, stops.stop_lon, stop_times.stop_sequence, stop_times.shape_dist_traveled, stop_times.trip_id, stop_times.arrival_time, stop_times.departure_time
+                FROM stop_times
+                INNER JOIN stops ON stop_times.stop_id = stops.stop_id
+                WHERE stop_times.trip_id IS '{0}'
+            """.format(queryParam))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+            raise
+
+        connection.close()
+        return result
+
+
 class Dates(tornado.web.RequestHandler):
     def post(self):
         queryParam = self.get_argument("tripID").encode('utf-8')
-        response = executeDateQuery(queryParam)
+        response = self.executeDateQuery(queryParam)
         data = []
         for a, b in response:
             item = {}
@@ -113,10 +202,33 @@ class Dates(tornado.web.RequestHandler):
 
         self.write(json.dumps(data))
 
+    def executeDateQuery(self, queryParam):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
+
+        try:
+            cursorobj.execute("""
+                SELECT trips.trip_id, calendar_dates.date
+                FROM trips
+                INNER JOIN calendar_dates ON trips.service_id = calendar_dates.service_id
+                WHERE trips.trip_id IS '{0}'
+                ORDER BY calendar_dates.date
+            """.format(queryParam))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+                raise
+
+        connection.close()
+        return result
+
+
 class StopTimes(tornado.web.RequestHandler):
     def post(self):
-        queryParam = self.get_argument("stopID").encode('utf-8')
-        response = executeStopTimesQuery(queryParam)
+        queryParam1 = self.get_argument("tripID").encode('utf-8')
+        queryParam2 = self.get_argument("stopID").encode('utf-8')
+        response = self.executeStopTimesQuery(queryParam1, queryParam2)
         data = []
         for a, b, c in response:
             item = {}
@@ -127,130 +239,25 @@ class StopTimes(tornado.web.RequestHandler):
 
         self.write(json.dumps(data))
 
-# Fetch all stops from a specific trip
-def executeTripStopsQuery(queryParam):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
+    def executeStopTimesQuery(self, queryParam1, queryParam2):
+        dbPath = "db.sqlite"
+        connection = sqlite3.connect(dbPath)
+        cursorobj = connection.cursor()
 
-    try:
-        print queryParam
-        cursorobj.execute("""
-            SELECT stops.stop_id, stops.stop_name, stops.stop_lat, stops.stop_lon, stop_times.stop_sequence, stop_times.shape_dist_traveled
-            FROM stop_times
-            INNER JOIN stops ON stop_times.stop_id = stops.stop_id
-            WHERE stop_times.trip_id IS '{0}'
-        """.format(queryParam))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-        raise
+        try:
+            cursorobj.execute("""
+                SELECT stop_times.stop_id, stop_times.arrival_time, stop_times.departure_time
+                FROM stop_times
+                WHERE stop_times.trip_id IS '{0}' AND stop_times.stop_id IS '{1}'
+                ORDER BY stop_times.stop_id
+            """.format(queryParam1, queryParam2))
+            result = cursorobj.fetchall()
+            connection.commit()
+        except Exception:
+                raise
 
-    connection.close()
-    return result
-
-# Fetch all trips with specific route id
-def executeTripQuery(queryParam):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
-
-    try:
-        cursorobj.execute("""
-            SELECT trips.trip_id, trips.trip_headsign
-            FROM trips
-            INNER JOIN routes ON routes.route_id = trips.route_id
-            WHERE trips.route_id IS '{0}'
-        """.format(queryParam))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-        raise
-
-    connection.close()
-    return result
-
-# Fetch all routes by its long name
-def executeRouteQuery(queryParam1, queryParam2, queryParam3, queryParam4):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
-    print queryParam1, queryParam2, queryParam3, queryParam4
-    try:
-        cursorobj.execute("""
-            SELECT route_id, agency_name, route_short_name, route_long_name, agency_url
-            FROM routes
-            INNER JOIN agency ON routes.agency_id = agency.agency_id
-            WHERE route_long_name LIKE '%{0}%' AND route_id LIKE '%{1}%' AND agency_name LIKE '%{2}%' AND route_short_name LIKE '%{3}%'
-            ORDER BY route_id
-        """.format(queryParam1, queryParam2, queryParam3, queryParam4))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-        raise
-
-    connection.close()
-    return result
-
-# Fetch all stops by its name
-def executeStopQuery(queryParam1, queryParam2):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
-
-    try:
-        cursorobj.execute("""
-            SELECT stop_id, stop_name, stop_lat, stop_lon
-            FROM stops
-            WHERE stop_id LIKE '%{0}%' AND stop_name LIKE '%{1}%'
-            ORDER BY stop_id
-        """.format(queryParam1, queryParam2))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-            raise
-
-    connection.close()
-    return result
-
-def executeDateQuery(queryParam):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
-
-    try:
-        cursorobj.execute("""
-            SELECT trips.trip_id, calendar_dates.date
-            FROM trips
-            INNER JOIN calendar_dates ON trips.service_id = calendar_dates.service_id
-            WHERE trips.trip_id IS '{0}'
-        """.format(queryParam))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-            raise
-
-    connection.close()
-    return result
-
-def executeStopTimesQuery(queryParam):
-    dbPath = "db.sqlite"
-    connection = sqlite3.connect(dbPath)
-    cursorobj = connection.cursor()
-
-    try:
-        cursorobj.execute("""
-            SELECT stop_times.stop_id, stop_times.arrival_time, stop_times.departure_time
-            FROM stop_times
-            WHERE stop_times.stop_id IS '{0}'
-        """.format(queryParam))
-        result = cursorobj.fetchall()
-        connection.commit()
-    except Exception:
-            raise
-
-    connection.close()
-    return result
+        connection.close()
+        return result
 
 # def load_gtfs():
 #     # print(dao)
