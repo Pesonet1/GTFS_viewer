48c63bc48e11ac5235dab576ae4a5edd0d9c9ed8
Petteri Pesonen <Petteri.Pesonen@karttakeskus.fi>
2017-07-27 09:44:12 +0300

    Individual trip calendar dates can be viewed

--- handlers/index.py
+++ handlers/index.py
@@ -5,6 +5,8 @@ from tornado.escape import json_decode
 import json
 import pprint
 import sqlite3
+import csv
+import os
 
 from gtfslib.dao import Dao
 
@@ -57,7 +59,7 @@ class Routes(tornado.web.RequestHandler):
                 FROM routes
                 INNER JOIN agency ON routes.agency_id = agency.agency_id
                 WHERE route_long_name LIKE '%{0}%' AND route_id LIKE '%{1}%' AND agency_name LIKE '%{2}%' AND route_short_name LIKE '%{3}%'
-                ORDER BY route_id
+                ORDER BY route_id, agency_name, route_short_name
             """.format(queryParam1, queryParam2, queryParam3, queryParam4))
             result = cursorobj.fetchall()
             connection.commit()
@@ -117,15 +119,25 @@ class Stops(tornado.web.RequestHandler):
 class Trips(tornado.web.RequestHandler):
     def post(self):
         queryParam = self.get_argument("routeID").encode('utf-8')
-        response = self.executeTripQuery(queryParam)
-        data = []
-        for a, b in response:
+        trips = self.executeTripQuery(queryParam)
+        urls = self.getKoontikantaUrl()
+
+        trip_data = []
+        for a, b in trips:
             item = {}
             item['trip_id'] = a
             item['trip_headsign'] = b
-            data.append(item)
+            trip_data.append(item)
 
-        self.write(json.dumps(data))
+        url_data = []
+        for a in urls:
+            item = {}
+            item['vuoron_url_interpoloitu'] = a
+            url_data.append(item)
+
+        # print url_data
+
+        self.write(json.dumps(trip_data))
 
     def executeTripQuery(self, queryParam):
         dbPath = "db.sqlite"
@@ -136,7 +148,6 @@ class Trips(tornado.web.RequestHandler):
             cursorobj.execute("""
                 SELECT trips.trip_id, trips.trip_headsign
                 FROM trips
-                INNER JOIN routes ON routes.route_id = trips.route_id
                 WHERE trips.route_id IS '{0}'
             """.format(queryParam))
             result = cursorobj.fetchall()
@@ -146,6 +157,23 @@ class Trips(tornado.web.RequestHandler):
 
         connection.close()
         return result
+
+    # def getKoontikantaUrl(self):
+    #     dbPath = "vallu.sqlite"
+    #     connection = sqlite3.connect(dbPath)
+    #     cursorobj = connection.cursor()
+    #     # trip_id = vuorotunniste_pysyva
+    #     # route_id = reittinro_pysyva
+    #     # agency_id = liikharjnro
+    #     try:
+    #         cursorobj.execute(""" SELECT vuoron_url_interpoloitu FROM vallu_vuorot """)
+    #         result = cursorobj.fetchall()
+    #         connection.commit()
+    #     except Exception:
+    #             raise
+    #
+    #     connection.close()
+    #     return result
 
 
 class TripStops(tornado.web.RequestHandler):
@@ -262,3 +290,55 @@ class StopTimes(tornado.web.RequestHandler):
 # def load_gtfs():
 #     # print(dao)
 #     dao.load_gtfs("matka_gtfs.zip")
+
+# def createTable():
+#     con = sqlite3.connect("vallu.sqlite")
+#     cur = con.cursor()
+#     cur.execute("CREATE TABLE vallu_vuorot (lu_viranro_myontaa, viranomaisnimi, lu_viranro_valvoo, viranomaisnimi_1, lu_voim_pvm, lu_lop_pvm, lu_tod_loppvm, lupasoptunnus, muokattu_pvm, liikharjnro, liikharj_nimi, reittinro_pysyva, reittinimi, ajosuunta, linjan_tunnus, reitti_voimaan_pvm, reitti_paattyy_pvm, reittia_muokattu_pvm, vuorotunniste_pysyva, vuoromerk, lahtoaika, perilla, kausi, vuorotyyppi, vuoro_lisatunniste, vuoro_voimaan_pvm, vuoro_paattyy_pvm, vuoroa_muokattu_pvm, kasitelty_koontikartassa, siirtyy_matka_fi, vuoron_url_interpoloitu);")
+#
+#     con.commit()
+#     con.close()
+#
+# def insertTable():
+#     con = sqlite3.connect("vallu.sqlite")
+#     cur = con.cursor()
+#
+#     reader = csv.reader(open('handlers/vallu_vuorot.csv', 'rb'), delimiter=';')
+#     for row in reader:
+#         to_db = [
+#             unicode(row[0], 'utf-8'),
+#             unicode(row[1], 'utf-8'),
+#             unicode(row[2], 'utf-8'),
+#             unicode(row[3], 'utf-8'),
+#             unicode(row[4], 'utf-8'),
+#             unicode(row[5], 'utf-8'),
+#             unicode(row[6], 'utf-8'),
+#             unicode(row[7], 'utf-8'),
+#             unicode(row[8], 'utf-8'),
+#             unicode(row[9], 'utf-8'),
+#             unicode(row[10], 'utf-8'),
+#             unicode(row[11], 'utf-8'),
+#             unicode(row[12], 'utf-8'),
+#             unicode(row[13], 'utf-8'),
+#             unicode(row[14], 'utf-8'),
+#             unicode(row[15], 'utf-8'),
+#             unicode(row[16], 'utf-8'),
+#             unicode(row[17], 'utf-8'),
+#             unicode(row[18], 'utf-8'),
+#             unicode(row[19], 'utf-8'),
+#             unicode(row[20], 'utf-8'),
+#             unicode(row[21], 'utf-8'),
+#             unicode(row[22], 'utf-8'),
+#             unicode(row[23], 'utf-8'),
+#             unicode(row[24], 'utf-8'),
+#             unicode(row[25], 'utf-8'),
+#             unicode(row[26], 'utf-8'),
+#             unicode(row[27], 'utf-8'),
+#             unicode(row[28], 'utf-8'),
+#             unicode(row[29], 'utf-8'),
+#             unicode(row[30], 'utf-8')
+#         ]
+#         cur.execute("INSERT INTO vallu_vuorot (lu_viranro_myontaa, viranomaisnimi, lu_viranro_valvoo, viranomaisnimi_1, lu_voim_pvm, lu_lop_pvm, lu_tod_loppvm, lupasoptunnus, muokattu_pvm, liikharjnro, liikharj_nimi, reittinro_pysyva, reittinimi, ajosuunta, linjan_tunnus, reitti_voimaan_pvm, reitti_paattyy_pvm, reittia_muokattu_pvm, vuorotunniste_pysyva, vuoromerk, lahtoaika, perilla, kausi, vuorotyyppi, vuoro_lisatunniste, vuoro_voimaan_pvm, vuoro_paattyy_pvm, vuoroa_muokattu_pvm, kasitelty_koontikartassa, siirtyy_matka_fi, vuoron_url_interpoloitu) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
+#
+#     con.commit()
+#     con.close()
